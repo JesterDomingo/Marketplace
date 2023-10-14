b4cb95554f10207ab47cab3a291f64b5018eb492
Jester Domingo <jrndomingo@gmail.com>
2023-10-14 12:52:37 -0400

    Final commit | All features working

--- controller/marketplace.js
+++ controller/marketplace.js
@@ -1,119 +1,137 @@
-const product = require("../models/product")
+const product = require("../models/product");
+
+//Get add
+const addProduct = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { description } = req.body;
+    const { price } = req.body;
+    const { quantity } = req.body;
+    const { category } = req.body;
+
+    const newProduct = new product({
+      name: name,
+      description: description,
+      price: price,
+      quantity: quantity,
+      category: category,
+    });
+
+    const saveProducts = await newProduct.save();
+    res.status(201).json({ success: true, data: saveProducts });
+  } catch (error) {
+    res
+      .status(409)
+      .json({ success: false, data: [], error: "Item cannot be added." });
+  }
+};
 
 //Post all
 const getProduct = async (req, res) => {
-    try{
-        const products = await product.find()
-        res.status(200).json({success: true, data: products})
-    }catch(error){
-        res.status(409).json({success:false, data: [], error: error})
-    }
-}
+  try {
+    const products = await product.find();
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    res
+      .status(409)
+      .json({ success: false, data: [], error: "No products found" });
+  }
+};
 
 //Post id
 const getProducts = async (req, res) => {
-    const productId = req.params.productId
-    try{
-        const Product = await product.find({_id: productId})
-        res.status(200).json({success: true, data: Product})
-    }catch(error){
-        res.status(409).json({success:false, data: [], error: error})
-    }
-}
+  const productId = req.params.productId;
+  try {
+    const Product = await product.find({ _id: productId });
+    res.status(200).json({ success: true, data: Product });
+  } catch (error) {
+    res
+      .status(409)
+      .json({
+        success: false,
+        data: [],
+        error: "No products found with the specified id",
+      });
+  }
+};
+
+//Put id
+const updateProducts = async (req, res) => {
+  const productid = req.params.productId;
+  const { name } = req.body;
+  const { description } = req.body;
+  const { price } = req.body;
+  const { quantity } = req.body;
+  const { category } = req.body;
+
+  try {
+    const updateItem = await product.updateOne(
+      { _id: productid },
+      {
+        $set: {
+          name: name,
+          description: description,
+          price: price,
+          quantity: quantity,
+          category: category,
+        },
+      }
+    );
+    const updatedItemData = await product.find({ _id: productid });
+    res.status(201).json({ success: true, data: updatedItemData });
+  } catch (error) {
+    res
+      .status(409)
+      .json({
+        success: false,
+        data: [],
+        error: "Product Information cannot be updated",
+      });
+  }
+};
 
 //Delete all
-const deleteProduct = async(req, res) =>{
-    try{
-        const products = await product.deleteMany();
-        res.status(200).json({success: true, data: products})
-    }catch(error){
-        res.status(409).json({success:false, data: [], error: error})
-    }
-}
+const deleteProduct = async (req, res) => {
+  try {
+    const products = await product.deleteMany();
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    res
+      .status(409)
+      .json({ success: false, data: [], error: "Items not found." });
+  }
+};
 
 //Delete id
 const deleteProducts = async (req, res) => {
-    const productId = req.params.productId
-    try{
-        const Product = await product.deleteOne({_id: productId})
-        res.status(200).json({success: true, data: Product})
-    }catch(error){
-        res.status(409).json({success:false, data: [], error: error})
-    }   
-}
+  const productId = req.params.productId;
+  try {
+    const Product = await product.deleteOne({ name: productId });
+    res.status(200).json({ success: true, data: Product });
+  } catch (error) {
+    res
+      .status(409)
+      .json({ success: false, data: [], error: "Item not found." });
+  }
+};
 
 //Get using name
 const getProductName = async (req, res) => {
-    const name = req.query
-    const query = {}
-    if(name) query.name = name
-    try{
-        const display = await display.find({name})
-        res.status(200).json({success: true, data: display})
-    }catch(error){
-        res.status(409).json({success:false, data: [], error: error})
-    }   
-}
-
-//Put id
-const updateProducts = async (req, res) =>{
-        const productid = req.params.productId
-        const {name} = req.body
-        const {description} = req.body
-        const {price} = req.body
-        const {quantity} = req.body
-        const {category} = req.body
-
-    try{
-        const updateItem = await product.updateOne({_id: productid}, {$set: {
-            name: name,
-            description: description,
-            price: price,
-            quantity: quantity,
-            category: category
-        }})
-        
-        const updatedItemData = await product.find({_id: productid})
-        res.status(201).json({success: true, data: updatedItemData})
+    const keyword = req.params.keyword;
+    try {
+        const products = await product.find({ name: { $regex: keyword, $options: 'i' } });
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        res.status(409).json({ success: false, data: [], error: "Item/s cannot be found" });
     }
-    catch(error) {
-        res.status(409).json({success: false, data: [], error: error})
-    }
-}
-
-
-//Adding products
-const addProduct = async (req, res) => {
-    try{
-        const {name} = req.body
-        const {description} = req.body
-        const {price} = req.body
-        const {quantity} = req.body
-        const {category} = req.body
-
-        const newProduct = new product({
-            name: name,
-            description: description,
-            price: price,
-            quantity: quantity,
-            category: category
-        })
-
-        const saveProducts = await newProduct.save()
-        res.status(201).json({success: true, data: saveProducts})
-    }
-    catch(error) {
-        res.status(409).json({success: false, data: [], error: error})
-    }
-}
+};
 
 module.exports = {
-    addProduct, //post
-    getProduct, //get
-    getProducts, //get id
-    deleteProduct, //delete
-    deleteProducts, //delete id
-    updateProducts, //Put id
-    getProductName //Get name
-
-}
+  addProduct, //post
+  getProduct, //get
+  getProducts, //get id
+  deleteProduct, //delete
+  deleteProducts, //delete id
+  updateProducts, //Put id
+  getProductName, //Get name --broken
+};
--- routes/routes.js
+++ routes/routes.js
@@ -4,16 +4,19 @@ const {addProduct, getProduct, getProducts, deleteProduct, deleteProducts, updat
 
 router = express.Router();
 
-//GETS
-router.get("/products", getProduct)
-router.get("/products/:productId", getProducts)
-router.get("/products?name=[]", getProductName)
-//POST
-router.post("/products", addProduct)
-//PUT
-router.put("/products/:productId", updateProducts)
-//DELETE
-router.delete("/products", deleteProduct)
-router.delete("/products/:productId", deleteProducts)
+//GET FUNCTIONS
+router.get("/products", getProduct) //show all items
+router.get("/products/:productId", getProducts) //show specific item by id
+router.get("/product/:keyword", getProductName);//show specific item by name --broken
+
+//POST FUNCTION
+router.post("/products", addProduct) //add item
+
+//PUT FUNCTION
+router.put("/products/:productId", updateProducts) //update item based on id
+
+//DELETE FUNCTIONS
+router.delete("/products", deleteProduct) //deletes all product
+router.delete("/products/:productId", deleteProducts) //deletes product based on id
 
 module.exports = router
